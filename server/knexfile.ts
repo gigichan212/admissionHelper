7651e05c9e4da96db05e623b0d3f7ed772727482
Avis <acheng730804@protonmail.com>
2021-04-26 02:01:32 +0800

    Change knex file

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -26,7 +26,7 @@ module.exports = {
   test: {
     client: "postgresql",
     connection: {
-      host: process.env.POSTGRE_HOST,
+      host: process.env.POSTGRES_HOST,
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
