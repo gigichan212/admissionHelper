c4d80de1ebc88a9eb098c8b2c9cb631b739ef553
Avis <acheng730804@protonmail.com>
2021-04-25 21:18:30 +0800

    Change path in letsencrypt

--- server/init-letsencrypt.sh
+++ server/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cms.admissionhelper.site www.cms.admissionhelper.site)
+domains=(api.admissionhelper.online www.api.admissionhelper.online)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="chufufai@yahoo.com.hk" # Adding a valid address is strongly recommended
+email="acheng730804@protonmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
