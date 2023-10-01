20a9137ac63601eaf1aa0f97ab2555c5a7243090
“Gigi” <changigi.ch@gmail.com>
2023-10-01 12:01:35 -0400

    removed unused folder

--- java-server/src/main/java/io/tecky/Repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package io.tecky.Repository;
-
-import io.tecky.Entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users,Integer> {
-}
